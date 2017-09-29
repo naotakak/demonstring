65780350d3818e4c6aeb1a5d1e3c839ee246aa13
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-09-28 22:09:14 -0400

    done

--- demonstring.c
+++ demonstring.c
@@ -5,10 +5,10 @@ int main() {
   char str1[] = "AabcdefAabc";
   char str2[] = "Aabc";
   
-  printf("strchr (value): 'AabcdefAabc', 'A': %d/n", & strchr(* str1, 'A'));
-  printf("strchr (pointer): 'AabcdefAabc', 'A': %d/n", strchr(* str1, 'A'));
-  printf("strstr (value): 'AabcdefAabc', 'Aabc': %d\n", & strstr(* str1, * str2));
-  printf("strstr (pointer): 'AabcdefAabc', 'Aabc': %d\n", strstr(* str1, * str2));
+  printf("strchr (value): 'AabcdefAabc', 'A': %c\n",  * strchr( str1, 'A'));
+  printf("strchr (pointer): 'AabcdefAabc', 'A': %d\n", strchr( str1, 'A'));
+  printf("strstr (value of index 0): 'AabcdefAabc', 'Aabc': %c\n", * strstr( str1,  str2));
+  printf("strstr (pointer): 'AabcdefAabc', 'Aabc': %d\n", strstr( str1,  str2));
   
   return 0;
 }
